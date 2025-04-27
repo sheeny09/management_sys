86eb587d9064b1fdfdc00fb3e36cf6a5a7f47b3f
sheeny09 <sujithasheeny092005@gmail.com>
2025-04-27 11:52:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('./Models/db');
 
 // CORS middleware - allow requests from frontend
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://student-management-2-fmq2.onrender.com'], // React default port - added both localhost and IP
+  origin: ['http://localhost:3000', 'https://management-sys-1.onrender.com'], // React default port - added both localhost and IP
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
