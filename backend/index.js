96624a02b957a3c927d3a0f6eb63c94f13bc66e9
sheeny09 <sujithasheeny092005@gmail.com>
2025-04-29 22:33:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -50,3 +50,13 @@ process.on('SIGINT', () => {
     process.exit(0);
   });
 });
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  });
+}
