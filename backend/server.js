6480978bf73d1a889368ed42f87ac5b0b02b664c
sheeny09 <sujithasheeny092005@gmail.com>
2025-04-30 01:13:31 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const studentRoutes = require('./routes/studentRoutes');
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+console.log('MONGODB_URI from server.js:', process.env.MONGODB_URI);
+connectDB();
+
+app.use(cors({ origin: 'http://localhost:3000' })); // Allow requests from localhost:3000
+app.use(express.json());
+
+app.use('/students', studentRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
